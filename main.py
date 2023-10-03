6f7e37318b35d884f582247912530a4afd560656
Ahmet Kutay Karacair <akkaracair@gmail.com>
2023-10-03 21:36:54 +0300

    Used motor instead of native mongoDB library, completed mongoDB connection, created UserSchema demo

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-# main.py
-import os
 from fastapi import FastAPI
-from Explorari.routes.v1 import users
-from Database import setup_mongodb
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
+from routes.v1 import users
+from Database.mongoDBConnection import database
+from Configs import settings
 
 app = FastAPI()
 
-# MongoDB Configuration
-MONGO_URI = os.environ.get("MONGO_CONNECTION_STRING")
-DB_NAME = os.environ.get("MONGO_DB_NAME")
-
-# Setup MongoDB connection
-setup_mongodb(app, MONGO_URI, DB_NAME)
-
 # Include API routes
 app.include_router(users.router, prefix="/v1/users")
+
+
+# Dependency Injection for MongoDB connection
+@app.on_event("startup")
+async def startup_db_client():
+    await database.client.start_session()
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    await database.client.close()
